56daeac947653b483af97ecfa6685c1a17c28b8c
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-18 14:02:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,39 @@ const issues = document.querySelector("#issues");
 const nextBtn = document.querySelector("#load_next")
 const prevBtn = document.querySelector("#load_prev");
 
+let currentPage = 1;
+
 const displayIssues = async()=>{
 try {
     const response = await fetch("https://api.github.com/repositories/1296269/issues?page=$%7BpageNumberHere%7D&per_page=5");
 	const data = await response.json();
 
-	const heading = document.createElement('h1');
-	heading.innerText = `Page number ${data.number}`;
-	contain.append(heading);
+	issues.innerHTML = "";
+	contain.querySelector("h1").innerText = `Page number ${currentPage}`;
+
+	if(data.length === 0){
+		issues.innerHTML = "<li>No issues fount on this page.</li>";
+		return;
+	}
+
+	data.forEach(issues =>{
+		const li = document.createElement("li");
+		li.textContent = issues.title;
+		issues.appendChild(li);
+	});
 	
 } catch (error) {
-	
+	console.error("Error fetching issues:",error);
+	issues.innerHTML = "<li>Failed to load issues. Please try again.</li>";
 }
 	
-}
+};
+nextBtn.addEventListener("click", ()=>{
+	if(currentPage>1){
+		currentPage--;
+		displayIssues();
+	}
+});
+
+contain.innerHTML = "<h1>Page number 1</h1>";
+displayIssues();
